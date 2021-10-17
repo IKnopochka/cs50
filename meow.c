ce4bb1b0e398da85fc1a8c442902f75a69854454
Irina Novakovich <irinaredhood@gmail.com>
2021-10-17 23:08:45 +0300

    Meow function prototype

--- meow.c
+++ meow.c
@@ -15,3 +15,18 @@ void meow(int n)
     printf("meow\n");
     }
 }
+
+void meow(void);
+
+int main (void)
+{
+    for (int i = 0; i <= 3; i++)
+    {
+        meow();
+    }
+}
+
+void meow(void)
+{
+    printf("meow\n");
+}
