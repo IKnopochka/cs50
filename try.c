471d5585911cf750fa7f4f727c0cea1d5c93c233
Irina Novakovich <irinaredhood@gmail.com>
2021-10-17 23:10:01 +0300

    points assigned to each letter of the alphabet

---
+++ try.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+char LETTERS[] = {'a', 'b', 'c', 'd', 'e', 'f', 'j', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+int main(void)
+{
+    string word1 = get_string("Player 1: ");
+    string x;
+    for (int i = 0, n = strlen(word1); i < n; i++)
+    {
+        x = islower(word1[i]);
+    }
+    printf("AFter: %s\n", x);
+
+}
